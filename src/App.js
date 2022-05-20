47e871dfbf9b6b7278dacdd6ede2b1f13e542651
Mariam Viera <mariamviera7@gmail.com>
2022-05-20 14:48:27 -0300

    Complete routes and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddProject from "./components/Administrator/AddProject";
+import EditProject from "./components/Administrator/EditProject";
+import ProjectList from "./components/ProjectList/ProjectList";
+import { ProjectProvider } from "./context/ProjectContext";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="text-start p-2 b border-bottom border-secondary">
+        <img src="../../logo.png" alt="logo"/>
       </header>
+      <ProjectProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<ProjectList />}/>
+            <Route path="/create" element={<AddProject/>}/>
+            <Route path="/edit/:id" element={<EditProject/>}/>
+          </Routes>
+        </Router>
+      </ProjectProvider>
     </div>
   );
 }
